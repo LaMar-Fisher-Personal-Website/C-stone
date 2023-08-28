65858a3c3348f7a10c651e6285a2eb2e02808d16
LaMar Fisher <lsfisher503@gmail.com>
2023-08-28 13:51:46 -0700

    Restored lost files, checkoutpage and cartpage and added to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import ProductList from './ProductList';
 import ShopCart from './ShopCart';
-import Login from './Login'; // Create a Login component
-import Checkout from './Checkout'; // Import the Checkout component
+import Login from './Login';
+import CartPage from './CartPage';
+import CheckoutPage from './CheckoutPage';
 
 function App() {
-    // State to manage the cart items
     const [cartItems, setCartItems] = useState([]);
     const [showCheckout, setShowCheckout] = useState(false);
     const [user, setUser] = useState(null);
@@ -15,76 +16,79 @@ function App() {
         if (storedCart) {
             setCartItems(JSON.parse(storedCart));
         }
-    }, []); 
-    
-    // Empty dependency array ensures this effect runs only once when the component mounts
+    }, []);
 
-    // Function to add an item to the cart
     const addToCart = (product) => {
-        // Check if the product is already in the cart
         const existingItem = cartItems.find(item => item.id === product.id);
 
         if (existingItem) {
-            // If the product is already in the cart, update its quantity
             const updatedCart = cartItems.map(item =>
                 item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
             );
             setCartItems(updatedCart);
         } else {
-            // If the product is not in the cart, add it with quantity 1
             setCartItems([...cartItems, { ...product, quantity: 1 }]);
         }
     };
 
     const handleLogin = (fakeToken) => {
-        // Here you would perform a login request to the API using the fake token
-        // If the login is successful, set the user state
-        setUser(fakeToken); // For demonstration purposes, setting user directly
+        setUser(fakeToken);
     };
 
     const handleLogout = () => {
-        // Clear user state and cart data
         setUser(null);
         setCartItems([]);
         localStorage.removeItem('cart');
     };
 
-    // Update local storage whenever cartItems change
     useEffect(() => {
         localStorage.setItem('cart', JSON.stringify(cartItems));
     }, [cartItems]);
 
     return (
-      <div>
-          {!user ? (
-              <Login onLogin={handleLogin} />
-          ) : (
-              <div>
-                  <h1>Welcome, Fullstack Academy Future Grads!</h1>
-                  <ProductList addToCart={addToCart} />
-                  <ShopCart
-                      cartItems={cartItems}
-                      setCartItems={setCartItems}
-                  />
-                  {showCheckout ? (
-                      <div>
-                          <Checkout
-                              cartItems={cartItems}
-                              setCartItems={setCartItems}
-                          />
-                          <button onClick={() => setShowCheckout(false)}>Back to Cart</button>
-                      </div>
-                  ) : (
-                      <button onClick={() => setShowCheckout(true)}>Proceed to Checkout</button>
-                  )}
-                  <button onClick={handleLogout}>Logout</button>
-              </div>
-          )}
-      </div>
-  );
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/cart">Cart</Link>
+                        </li>
+                        <li>
+                            <Link to="/checkout">Checkout</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path="/cart" element={<CartPage cartItems={cartItems} setCartItems={setCartItems} />} />
+                    <Route path="/checkout" element={<CheckoutPage cartItems={cartItems} setCartItems={setCartItems} />} />
+                    <Route path="/" element={!user ? <Login onLogin={handleLogin} /> : (
+                        <div>
+                            <h1>Welcome, FullStack Future Grads!</h1>
+                            <ProductList addToCart={addToCart} />
+                            <ShopCart cartItems={cartItems} setCartItems={setCartItems} />
+                            {showCheckout ? (
+                                <div>
+                                    {/* Rest of the code for Checkout */}
+                                </div>
+                            ) : (
+                                <button onClick={() => setShowCheckout(true)}>Proceed to Checkout</button>
+                            )}
+                            <button onClick={handleLogout}>Logout</button>
+                        </div>
+                    )} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
+
+
 
 
 
